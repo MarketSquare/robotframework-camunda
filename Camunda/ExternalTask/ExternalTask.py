07764c285b3725e88cc4c70bb13f6d6efa9e4cb3
Markus Stahl <markus.stahl@postadress.de>
2020-09-25 14:56:58 +0200

    implement stub for "complete task"

--- Camunda/ExternalTask/ExternalTask.py
+++ Camunda/ExternalTask/ExternalTask.py
@@ -1,35 +1,63 @@
 from robot.api.deco import library, keyword
 from robot.api.logger import librarylogger as logger
-from typing import Dict, Any
+from typing import List, Dict, Any
 from camunda.client.external_task_client import ExternalTaskClient
 
 
 @library(scope='GLOBAL', version='0.0.1')
 class ExternalTask:
 
+    EMPTY_STRING = ""
     KNOWN_TOPICS: Dict[str,Dict[str, Any]] = {}
     CAMUNDA_ENGINE_URL: str = None
+    RECENT_TASK_ID: str = EMPTY_STRING
 
     def __init__(self, camunda_engine_url: str = None):
         if camunda_engine_url:
             self.CAMUNDA_ENGINE_URL = camunda_engine_url
-
-    @keyword("Fetch and Lock")
-    def fetch_and_lock(self, topic: str):
-        external_task: ExternalTask = self._get_task_client(topic)
-        work_items = external_task.fetch_and_lock([topic])
-        if work_items:
-            logger.debug(f'Recived {len(work_items)} work_items from camunda engine for topic:\t{topic}')
-        else:
-            logger.debug(f'Received no work items from camunda engine for topic:\t{topic}')
-
-        return work_items
 
     @keyword("Set Camunda URL")
     def set_camunda_url(self, url: str):
         if not url:
             raise ValueError('Cannot set camunda engine url: no url given.')
         self.CAMUNDA_ENGINE_URL = url
+
+    @keyword("Fetch and Lock workloads")
+    def fetch_and_lock(self, topic: str) -> Dict:
+        external_task: ExternalTask = self._get_task_client(topic, automatically_create_client=True)
+        work_items: List[Dict] = external_task.fetch_and_lock([topic])
+        if work_items:
+            logger.debug(f'Recived {len(work_items)} work_items from camunda engine for topic:\t{topic}')
+        else:
+            logger.debug(f'Received no work items from camunda engine for topic:\t{topic}')
+
+        if not work_items:
+            external_task_id = self.EMPTY_STRING
+        else:
+            external_task_id = work_items[0].get('id')
+
+        if self.RECENT_TASK_ID and self.RECENT_TASK_ID != external_task_id:
+            logger.warn(f'Fetched from "{external_task_id}different task than before. '
+                        f'Before:\t{self.RECENT_TASK_ID}')
+        self.RECENT_TASK_ID = external_task_id
+
+        if not work_items:
+            return work_items
+        else:
+            return [item.get('variables') for item in work_items]
+
+    @keyword("Get recent task id")
+    def get_recent_task_id(self):
+        return self.RECENT_TASK_ID
+
+    @keyword("Complete task")
+    def complete(self, topic, task_id= RECENT_TASK_ID, result_set: Dict[str,Any] = None):
+        if not topic:
+            raise ValueError('Unable complete task, because no topic given')
+        external_task = self._create_task_client(topic)
+        external_task.complete(task_id, global_variables=result_set)
+
+
 
     def _create_task_client(self, topic: str) -> ExternalTaskClient:
         if not self.CAMUNDA_ENGINE_URL:
@@ -40,8 +68,15 @@ class ExternalTask:
             raise ValueError('No topic set')
         return ExternalTaskClient(topic, self.CAMUNDA_ENGINE_URL)
 
-    def _get_task_client(self, topic) -> ExternalTaskClient:
+    def _get_task_client(self, topic, automatically_create_client = False) -> ExternalTaskClient:
+        if not topic:
+            raise ValueError('Unable to retrieve client, because no topic given.')
+
         if not self.KNOWN_TOPICS.get(topic, None):
+            if not automatically_create_client:
+                raise ValueError(f'No client available for topic "{topic}". Either you misspelled the topic or you missed'
+                                 f' creating a client before.')
+
             new_task_client: ExternalTask = self._create_task_client(topic)
             self.KNOWN_TOPICS[topic] = {'client': new_task_client}
 
--- tests/robot/test_fetch_and_lock.robot
+++ tests/robot/test_fetch_and_lock.robot
@@ -2,7 +2,27 @@
 Library    ../../Camunda/ExternalTask/ExternalTask.py
 
 *** Test Cases ***
-Teste 'fetch and lock'
+Test 'fetch and lock' for non existing topic
     [Setup]    set camunda url    http://localhost:8080/engine-rest
-    ${work_item}    fetch and lock    topic=test
-    Should Be Empty    ${work_item}
+    # GIVEN
+    ${non_existing_topic}    Set Variable    asdqeweasdwe
+
+    # WHEN
+    ${work_items}    fetch and lock workloads   topic=${non_existing_topic}
+
+    # THEN
+    Should Be Empty    ${work_items}
+
+Test 'fetch and lock' for inacurrate camunda url
+    # GIVEN
+    ${invalid_camunda_url}    Set Variable    https://localhost:9212
+    set camunda url    ${invalid_camunda_url}
+
+    # WHEN
+    ${pass_message}    ${error}    Run Keyword and ignore error    fetch and lock workloads    topic=random
+
+    # THEN
+    Should Be Equal    FAIL    ${pass_message}
+    Should contain    ${error}    ConnectionError
+
+
