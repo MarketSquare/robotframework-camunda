b66cabc6edfacfe222e639c335831db9923b0b70
noordsestern <markus.i.sverige@gmail.com>
2020-11-18 23:15:55 +0100

    fix generation of keyword documentation

--- libdoc/generate_libdoc.py
+++ libdoc/generate_libdoc.py
@@ -1,12 +1,22 @@
 from robot.libdoc import libdoc
 import os
+import re
+
+module_name = 'CamundaLibrary'
+
+
+version_regex = r"^v(?P<version>\d*\.\d*\.\d*$)"
+version = os.environ.get('CI_COMMIT_TAG', f'0-{os.environ.get("CI_COMMIT_REF_NAME","dev")}')
+full_version_match = re.fullmatch(version_regex, version)
+if full_version_match:
+    version = full_version_match.group('version')
 
 def generate_libdoc():
-    with os.scandir('./CamundaLibrary') as dirs:
+    with os.scandir(f'./{module_name}') as dirs:
         for entry in dirs:
             filename, fileextenstion = os.path.splitext(entry.name)
             if '__init__' != filename and '.py' == fileextenstion:
-                libdoc(f'Camunda.{filename}', outfile=f'public/keywords/{filename.lower()}/index.html', version='0.3.4')
+                libdoc(f'{module_name}.{filename}', outfile=f'public/keywords/{filename.lower()}/index.html', version=version)
 
 if __name__ == '__main__':
     generate_libdoc()
