42a6a3ca56b68c9860dc7ba046bd701d81ca8dce
noordsestern <markus.i.sverige@gmail.com>
2021-02-10 21:12:14 +0100

    documentation

--- CamundaLibrary/CamundaLibrary.py
+++ CamundaLibrary/CamundaLibrary.py
@@ -19,6 +19,49 @@ from .CamundaResources import CamundaResources
 
 @library(scope='GLOBAL')
 class CamundaLibrary:
+    """
+    Library for Camunda integration in Robot Framework
+
+    = Usage =
+
+    The library provides convenience keywords for accessing Camunda via REST API. You may deploy models,
+    start processes, fetch workloads and complete them.
+
+    When initializing the library the default url for Camunda is `http://localhost:8080` which is the default when
+    running Camunda locally. It is best practice to provide a variable for the url, so it can be set dynamically
+    by the executing environment (like on local machine, in pipeline, on test system and production):
+
+    | Library | CamundaLibrary | ${CAMUNDA_URL} |
+
+    Running locally:
+    | robot -v "CAMUNDA_URL:http://localhost:8080" task.robot
+
+    Running in production
+    | robot -v "CAMUNDA_URL:https://camunda-prod.mycompany.io" task.robot
+
+    = Execution =
+
+    In contrast to external workers that are common for Camunda, tasks implemented with CamundaLibrary do not
+    _subscribe_ on certain topics. A robot tasks is supposed to run once. How frequent the task is executed is up
+    to the operating environment of the developer.
+
+    == Subscribing a topic / long polling ==
+    You may achieve a kind of subscription by providing the ``asyncResponseTimeout`` with the `Fetch workload`
+    keyword in order to achieve [https://docs.camunda.org/manual/7.14/user-guide/process-engine/external-tasks/#long-polling-to-fetch-and-lock-external-tasks|Long Polling].
+
+    | ${variables} | fetch workload | my_topic | asyncResponseTimeout=60000 |
+    | log | Waited at most 1 hour before this log statement got executed |
+
+    = Missing Keywords =
+
+    If you miss a keyword, you can utilize the REST API from Camunda by yourself using the [https://github.com/MarketSquare/robotframework-requests|RequestsLibrary].
+    With RequestsLibrary you can access all of the fully documented [https://docs.camunda.org/manual/latest/reference/rest/|Camunda REST API].
+
+    = Feedback =
+
+    Feedback is very much appreciated regardless if it is comments, reported issues, feature requests or even merge
+    requests. You are welcome to participating in any way at the [https://gitlab.com/postadress/robotframework/robotframework-camunda|GitLab project of CamundaLibrary].
+    """
 
     WORKER_ID = f'robotframework-camundalibrary-{time.time()}'
 
@@ -41,7 +84,7 @@ class CamundaLibrary:
             raise ValueError('Cannot set camunda engine url: no url given.')
         self._shared_resources.camunda_url = f'{url}/engine-rest'
 
-    @keyword(name='Deploy model from file')
+    @keyword(name='Deploy model from file', tags=['deployment'])
     def deploy_bpmn(self, path_bpmn_file: str):
         """Uploads a camunda model to camunda that is provided as path.
 
@@ -76,7 +119,7 @@ class CamundaLibrary:
 
         return response.to_dict()
 
-    @keyword("Fetch and Lock workloads")
+    @keyword("Fetch and Lock workloads", tags=['task', 'deprecated'])
     def fetch_and_lock_workloads(self, topic, **kwargs) -> Dict:
         """*DEPRECATED*
 
@@ -85,7 +128,7 @@ class CamundaLibrary:
         logger.warn('Keyword "Fetch and Lock workloads" is deprecated. Use "Fetch workload" instead.')
         return self.fetch_workload(topic, **kwargs)
 
-    @keyword("Fetch Workload")
+    @keyword("Fetch Workload", tags=['task'])
     def fetch_workload(self, topic: str, **kwargs) -> Dict:
         """
         Locks and fetches workloads from camunda on a given topic. Returns a list of variable dictionary.
@@ -146,7 +189,7 @@ class CamundaLibrary:
         variables: Dict[str, VariableValueDto] = self.FETCH_RESPONSE.variables
         return CamundaResources.convert_openapi_variables_to_dict(variables)
 
-    @keyword("Get recent process instance")
+    @keyword("Get recent process instance", tags=['task', 'deprecated'])
     def get_process_instance_id(self):
         """*DEPRECATED*
         _Use `get fetch response` instead and check for ``process_instance_id`` element on the fetch reponse:
@@ -167,18 +210,52 @@ class CamundaLibrary:
             return self.FETCH_RESPONSE.process_instance_id
         return self.EMPTY_STRING
 
-    @keyword("Get fetch response")
+    @keyword("Get fetch response", tags=['task'])
     def get_fetch_response(self):
+        """Returns cached response from the last call of `fetch workload`.
+
+        The response contains all kind of data that is required for custom REST Calls.
+
+        Example:
+            | *** Settings *** |
+            | *Library* | RequestsLibrary |
+            | |
+            | *** Tasks *** |
+            | | *Create Session* | _alias=camunda_ | _url=http://localhost:8080_ |
+            | | ${variables} | *fetch and lock workloads* | _my_first_task_in_demo_ | |
+            | | ${fetch_response} | *get fetch response* | | |
+            | | *POST On Session* | _camunda_ | _engine-rest/external-task/${fetch_response}[id]/complete_ | _json=${{ {'workerId': '${fetch_response}[worker_id]'} }}_ |
+        """
         if self.FETCH_RESPONSE:
             return self.FETCH_RESPONSE.to_dict()
         return self.EMPTY_STRING
 
-    @keyword("Complete task")
+    @keyword("Complete task", tags=['task'])
     def complete(self, result_set: Dict[str, Any] = None, files: Dict = None):
         """
-        Completes recent task.
+        Completes the task that was fetched before with `fetch workload`.
 
-        result_set must be a dictionary like: {'key' : 'value'}
+        *Requires `fetch workload` to run before this one, logs warning instead.*
+
+        Additonal variables can be provided as dictionary in _result_set_ .
+        Files can be provided as dictionary of filename and patch.
+
+        Examples:
+
+            | _# fetch and immediately complete_ |
+            | | *fetch workload* | _my_topic_ |
+            | | *complete task* | |
+            | |
+            | _# fetch and complete with return values_ |
+            | | *fetch workload* | _decide_on_dish_ |
+            | ${new_variables} | *Create Dictionary* | _my_dish=salad_ |
+            | | *complete task* | _result_set=${new_variables}_ |
+            | |
+            | _# fetch and complete with return values and files_ |
+            | | *fetch workload* | _decide_on_haircut_ |
+            | ${return_values} | *Create Dictionary* | _style=short hair_ |
+            | ${files} | *Create Dictionary* | _should_look_like=~/favorites/beckham.jpg_ |
+            | | *complete task* | _${return_values}_ | _${files}_ |
         """
         if not self.FETCH_RESPONSE:
             logger.warn('No task to complete. Maybe you did not fetch and lock a workitem before?')
@@ -196,7 +273,7 @@ class CamundaLibrary:
                 except ApiException as e:
                     logger.error(f"Exception when calling ExternalTaskApi->complete_external_task_resource: {e}\n")
 
-    @keyword("Download file from variable")
+    @keyword("Download file from variable", tags=['task'])
     def download_file_from_variable(self, variable_name: str) -> str:
         if not self.FETCH_RESPONSE:
             logger.warn('Could not download file for variable. Maybe you did not fetch and lock a workitem before?')
@@ -212,8 +289,7 @@ class CamundaLibrary:
                     logger.error(f"Exception when calling ExternalTaskApi->get_process_instance_variable_binary: {e}\n")
                 return response
 
-
-    @keyword("Unlock")
+    @keyword("Unlock", tags=['task'])
     def unlock(self):
         """
         Unlocks recent task.
@@ -228,7 +304,7 @@ class CamundaLibrary:
                 except ApiException as e:
                     logger.error(f"Exception when calling ExternalTaskApi->unlock: {e}\n")
 
-    @keyword("Start process")
+    @keyword("Start process", tags=['process'])
     def start_process(self, process_key: str, variables: Dict = None, files: Dict = None,
                       before_activity_id: str = None, after_activity_id: str = None) -> Dict:
         """
@@ -284,7 +360,7 @@ class CamundaLibrary:
 
         return response.to_dict()
 
-    @keyword("Delete process instance")
+    @keyword("Delete process instance", tags=['process'])
     def delete_process_instance(self, process_instance_id):
         """
         USE WITH CARE: Deletes a process instance by id. All data in this process instance will be lost.
@@ -298,7 +374,7 @@ class CamundaLibrary:
                 logger.error(f'Failed to delete process instance {process_instance_id}:\n{e}')
                 raise e
 
-    @keyword("Get all active process instances")
+    @keyword("Get all active process instances", tags=['process'])
     def get_all_process_instances(self, process_definition_key):
         """
         Returns a list of process instances that are active for a certain process definition identified by key.
