becd40c4017a328201673ead3d4ede076a047e94
Markus Stahl <markus.stahl@postadress.de>
2021-02-12 16:23:11 +0100

    add use_priority parameter
    because we can

--- CamundaLibrary/CamundaLibrary.py
+++ CamundaLibrary/CamundaLibrary.py
@@ -140,7 +140,7 @@ class CamundaLibrary:
         return self.fetch_workload(topic, **kwargs)
 
     @keyword("Fetch Workload", tags=['task'])
-    def fetch_workload(self, topic: str, async_response_timeout=None, **kwargs) -> Dict:
+    def fetch_workload(self, topic: str, async_response_timeout=None, use_priority=None, **kwargs) -> Dict:
         """
         Locks and fetches workloads from camunda on a given topic. Returns a list of variable dictionary.
         Each dictionary representing 1 workload from a process instance.
@@ -180,6 +180,7 @@ class CamundaLibrary:
             topic_dto=FetchExternalTaskTopicDto(topic_name=topic, **kwargs)
             fetch_external_tasks_dto = FetchExternalTasksDto(worker_id=self.WORKER_ID, max_tasks=1,
                                                              async_response_timeout=async_response_timeout,
+                                                             use_priority=use_priority,
                                                              topics=[topic_dto])
 
             try:
